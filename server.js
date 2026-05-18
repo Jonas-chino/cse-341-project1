1c34e606a8933ee21fea66b5e831844b07057eb0
jonas chino <chinojonas99@gmail.com>
2026-05-18 00:03:32 -0400

    finishweek1

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require("express");
+
+const mongobd = require("./data/database");
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
+
+mongobd.initDb((err) =>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        app.listen(port, () =>(console.log(`Database is listening and node is Runing on port ${port}`)));
+    }
+})
 app.listen(port, () => {console.log(`runing on port ${port}`)});
